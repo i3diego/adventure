8c4e412238b83d84d037f9ab2b14bc32d3e56ce9
Diego <diego.parejo.carrasco@gmx.de>
2020-08-23 22:59:56 +0200

    New. Created main.py. Contains main game loop, and many mockups for now, also contains input handling

---
+++ main.py
@@ -0,0 +1,84 @@
+import sys
+from room import Room
+
+"""
+Main game loop lives here.
+"""
+commands = ["left", "right", "forward", "backward", "inspect", "light", "help"]
+
+print('This is a text based adventure')
+print('Your commands can be "left", "right", "forward", "backward", "inspect", "light" or "help" if you need a reminder')
+
+
+sides_entrance = {
+    "left" : 'wall',
+    "right" : 'door',
+    "front" : 'wall',
+    "back" : 'wall'
+}
+
+sides_r1 = {
+    "left" : 'door',
+    "right" : 'wall',
+    "front" : 'wall',
+    "back" : 'wall'
+}
+
+entrance = Room(sides_entrance,'light', [], 'entrance', (0,0))
+room1 = Room(sides_r1, 'light', [], 'room 1', (0,1))
+current_room = None
+
+map = [[entrance, room1]]
+
+def quitGame():
+    print("I'm quitting")
+    sys.exit()
+
+def move(**kwargs):
+    if (kwargs.current_room[kwargs.direction] == 'door'):
+        current_room = kwargs.next_room
+        print('you have entered '+ current_room.name)
+        return
+    if (current_room[kwargs.direction] == 'wall'):
+        print('you hit yourself on a wall "BONK"')
+        return
+
+def inspect(**kwargs):
+    print('insepection')
+    #print(kwargs.current_room.objects)
+
+def printHelp():
+    print(' You may use the following commands: ')
+    print(commands)
+
+def light():
+    print('Torch lit')
+
+switcher = {
+   "left"    : move,
+   "right"   : move,
+   "forward" : move,
+   "backward": move,
+   "inspect" : inspect,
+   "help"    : printHelp,
+   "light"   : light,
+   "quit"    : quitGame    
+} 
+
+def evalCommand(comand, **kwargs):
+    try:
+        return switcher[command]()
+    except KeyError as err:
+        print('unknow command')
+    
+
+print('Welcome try to escape')
+current_room = entrance
+
+while 1 < 2:
+    print("You are currently here " +current_room.name)
+    command = input("Enter command: ")
+    action = evalCommand(command, current_room = current_room, )
+    print(map[0][1].name)
+
+

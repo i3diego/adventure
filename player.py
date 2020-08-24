c5136fcf508f934a9ba35b1b41d985f2df529180
Diego <diego.parejo.carrasco@gmx.de>
2020-08-24 15:12:14 +0200

    Edit: Changed postion to single coordinates ( x, y)

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 class Player:
     def __init__(self):
-        self.postion = [0,0]
+        self.x = 0
+        self.y = 0
         self.inventory = { 
             'torch': 0,
             'keys': 0,

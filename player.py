173d247101b2d68033d27cd07f3bf196bce0fba8
Diego <diego.parejo.carrasco@gmx.de>
2020-08-26 22:25:21 +0200

    Edit: Added get/set and Update functions to player class

--- player.py
+++ player.py
@@ -6,3 +6,21 @@ class Player:
             'torch': 0,
             'keys': 0,
         }
+
+    def getX(self):
+        return self.x
+
+    def setX(self, new_x):
+        self.x = new_x
+    
+    def updateX(self, val):
+        self.x += val
+
+    def updateY(self, val):
+        self.y += val
+
+    def getY(self):
+        return self.y
+    
+    def setY(self, new_y):
+        self.y = new_y

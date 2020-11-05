cacc753de254bf070dbdf6bd2beae80be8c03a31
Diego <diego.parejo.carrasco@gmx.de>
2020-11-05 20:48:45 +0100

    Reworked Room

--- room.py
+++ room.py
@@ -1,3 +1,5 @@
+from random import random
+
 class Room:
     """
     This class defines a room, which the player may interact with. Many rooms can make up a maze like structure, through wchich one has to navigate.
@@ -5,26 +7,42 @@ class Room:
     Lastly a room can either be light or dark.
 
     """
-    def __init__(self, sides, light, objects, name, coordinates):
-        self.sides = sides
-        self.light = light
-        self.objects = objects
-        self.name = name
-        self.coordinates = coordinates
+    def __init__(self, north=(None,None), east =(None,None), south=(None,None), west=(None,None)):
+        # tuple v1 room refrence, v2 locked or not bool
+        self.north = (None,None)
+        self.east  = (None,None)
+        self.south = (None,None)
+        self.west  = (None,None)
+        self.neighbours ={}
+        self.iluminated = True
+        self.loot = []
 
-def randomizeRoom():
-    pass
+    def populate(self):
+        items = {
+            "torch" : 0.5,
+            "Small Key": 0.5
+        }
+        #[item for item in items.keys() if random() <= items[item]]
+        self.loot = [item for item, chance in items.items() if random() <= chance]
+        self.iluminated = random() <= 0.7
 
-def createSides():
-    pass
+    def add_neighbour(self, direction, room):
+        reverse_direction = {
+            'north' : 'south',
+            'east' : 'west',
+            'south' : 'north',
+            'west' : 'east'
+        }
+        if direction not in self.neighbours and reverse_direction[direction] not in room.neighbours:
+            self.neighbours[direction] = room
+            room.neighbours[reverse_direction[direction]] = self
 
-def light():
-    pass
+    def occupied_directions(self):
+        return list(self.neighbours.keys())
 
-def createObjects():
-    pass
+    def __repr__(self):
+       return f"Room id:{id(self)} neighbours:{self.neighbours}"
 
-def nameRoom():
-    pass
-
-
+    def __str__(self):
+        return f"Room id:{id(self)} neighbours:{self.neighbours}"
+        
